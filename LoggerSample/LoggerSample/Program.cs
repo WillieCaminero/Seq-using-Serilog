4448160c888804b1b3083f53289a067b8bfee5c6
W.Caminero@caribemedia.com <Willie Caminero@csid.local>
2018-03-05 14:14:41 -0400

    initial commit

---
+++ LoggerSample/LoggerSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using Serilog;
+using System;
+
+namespace LoggerSample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.Seq("http://localhost:5341")
+                .CreateLogger();
+
+            Log.Information("Hello, {Name}!", Environment.UserName);
+
+            // Important to call at exit so that batched events are flushed.
+            Log.CloseAndFlush();
+
+            Console.ReadKey(true);
+        }
+    }
+}
